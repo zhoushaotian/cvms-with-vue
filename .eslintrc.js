09a8c1826112b5db7a495e424e445d38437d1a0a
zhoushaotian <zhoushaotian@zbj.com>
2017-07-19 09:28:46 +0800

    "soucetype module"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
     "env": {
         "es6": true,
-        "node": true,
-        "browser": true,
-        "jquery": true
-    },
-    "parserOptions": {
-        "sourceType": "module"
+        "node": true
     },
     "extends": "eslint:recommended",
     "rules": {
